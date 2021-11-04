1d408066cfb7a6bab85a8bbce3ec1657e5fcecde
Ibrahim <ialakash@live.com>
2021-11-04 00:10:13 -0500

    fgs

---
+++ Mobile Web Frontend/forgot.js
@@ -0,0 +1,5 @@
+//Get the button:
+mybutton = document.getElementById("forgotPass");
+mybutton.onclick = function forgot(){
+    console.log("FORGOT PASSWORD")
+}
--- Mobile Web Frontend/login.js
+++ Mobile Web Frontend/login.js
@@ -1,32 +1,31 @@
-const mysql = require("mysql");
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config();
+// }
 
+const mysql = require("mysql");
 const express = require("express");
-const e = require("express");
 const bodyParser = require("body-parser");
 const encoder = bodyParser.urlencoded();
 const bcrypt = require("bcrypt");
-const passport = require("passport");
-const flash = require("express-flash");
-const session = require("express-session");
-const initializePassport = reqiure("./passport-config");
+const methodOverride = require("method-override");
 
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email)
-)
+var authcode;
+var registration_error = "";  
+var my_user_id = "";
 
 const app = express();
 app.set('view-engine', 'ejs');
 app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: false}));
-app.use(flash());
+app.use(methodOverride('_method'))
 
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     //port: "3306",
     password: "password",
-    database: "mytourney"
+    database: "mytourney_db",
+    timezone: 'utc'
 });
 
 //Connect to database
@@ -35,29 +34,30 @@ connection.connect(function(error){
     else console.log("Connected to database successfully")
 });
 
-app.get("/register", function(req, res){
-    res.render('register.ejs');
+app.get("/register", checkNotAuthenticatedReg, function(req, res){
+    res.render('register.ejs', {reg_error: registration_error});
 })
-app.get("/home", function(req, res){
+app.get("/home", checkAuthenticated, function(req, res){
     res.render('home.ejs');
 })
-app.get("/login", function(req, res){
+app.get("/login", checkNotAuthenticated, function(req, res){
     res.render('login.ejs');
+    registration_error = "";
 })
-app.get("/friends", function(req, res){
+app.get("/friends", checkAuthenticated, function(req, res){
     res.render('friends.ejs');
 })
-app.get("/profile_player", function(req, res){
+app.get("/profile_player", checkAuthenticated, function(req, res){
     res.render('profile_player.ejs');
 })
-app.get("/balance", function(req, res){
+app.get("/balance", checkAuthenticated, function(req, res){
     res.render('balance.ejs');
 })
-app.get("/team_player", function(req, res){
+app.get("/team_player", checkAuthenticated, function(req, res){
     res.render('team_player.ejs');
 })
 
-app.get("/", function(req, res){
+app.get("/", checkAuthenticated, function(req, res){
     res.render('home.ejs');
 })
 
@@ -66,11 +66,17 @@ app.get("/", function(req, res){
 //Database (USER TABLE): user_name, user_pass, user_email, user_first, user_gender, age, bio, photo, points, tourneys_played, tourneys_won, tourneys_lost, games_played, games_lost, goals_for, goals_against, shots, saves, shutouts
 //Need to add team many-to-many relationship, games_won, and user_last
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'; if nodejs is showing "consider upgrading mysql client"
+
 app.post("/login",encoder, function(req,res){
     var email = req.body.email;
     var password = req.body.password;
-    connection.query("select * from users where user_email = ? and user_pass = ?",[email, password], function(error, results, fields){
-        if (results.length > 0){
+    console.log(password);
+    connection.query("select * from users where user_email = ?",[email, password], function(error, results, fields){
+        console.log(results);
+        if (bcrypt.compare(password, results[0].user_pass)){
+            authcode = true;
+            my_user_id = results[0].user_id;
+            console.log(my_user_id);
             res.redirect("/home");
         }else {
             res.redirect("/login");
@@ -80,9 +86,10 @@ app.post("/login",encoder, function(req,res){
 })
 
 //When login is success
-app.get("/home", function(req, res){
+app.get("/home", checkAuthenticated, function(req, res){
     res.render('home.ejs');
 })
+
 
 app.post("/register", async function(req,res){
     var email = req.body.email;
@@ -114,18 +121,23 @@ app.post("/register", async function(req,res){
             user_first: first,
             user_last: last,
             user_gender: gender,
-            age: age};
+            age: birth};
 
-        //Columns: "user_name", "user_pass", "user_email", "user_first", "user_last", "user_gender", "age", "bio", "photo", "points", "tourneys_played", "tourneys_won", "tourneys_lost", "games_played", "games_lost", "goals_for", "goals_against", "shots", "saves", "shutouts"
+        //Columns: "user_name", "user_pass", "user_email", "user_first", "user_last", "user_gender", "age", "bio", "photo", "points", "tourneys_played", "tourneys_won", "tourneys_lost", "games_played", "games_won", "games_lost", "goals_for", "goals_against", "shots", "saves", "shutouts"
         var query = connection.query('INSERT INTO users SET ?',vals, function (error, results, fields){
             console.log(query);
-            console.log(vals)
-            console.log("USER REGISTERED");
+            console.log(vals);
+            
         })
         connection.query("select * from users where user_email = ? and user_pass = ?",[email, password], function(error, results, fields){
             if (results.length > 0){
                 res.redirect("/home");
+                console.log("USER REGISTERED");
+                authcode = true;
+                my_user_id = results[0].user_id;
+                registration_error = ""
             }else {
+                registration_error = "A USER WITH THAT EMAIL ALREADY EXISTS"
                 res.redirect("/register");
             }
             res.end();
@@ -135,6 +147,36 @@ app.post("/register", async function(req,res){
         res.redirect("/register");
     }
 })
+
+
+app.get('/logout', (req, res)=>{
+    authcode = false;
+    res.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){
+    if (authcode){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if (authcode){
+        return res.redirect('/home')
+    }
+    next()
+}
+function checkNotAuthenticatedReg(req, res, next){
+    if (authcode){
+        return res.redirect('/home')
+    }
+    next()
+}
+
+
+
+
 //16:31
 //'168.5.180.127' || 'localhost'
 // Start mysql server: "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqld" --console

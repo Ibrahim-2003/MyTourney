4c917bf17a878ff722a6430659611a6e98e9cd48
Ibrahim <ialakash@live.com>
2022-01-02 23:19:36 -0600

    fsdgdgdg

--- Mobile Web Frontend/sw.js
+++ Mobile Web Frontend/sw.js
@@ -1,15 +1,1 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('fox-store').then((cache) => cache.addAll([
-        'images/MyTourney Logo.png',
-        'images/icon.png'
-      ])),
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
-  });
+console.log('HAHAHAHAHAHAH')

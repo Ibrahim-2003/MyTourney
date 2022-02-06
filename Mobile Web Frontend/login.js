5f3030cdbe529dbb174984ae16865acb7d0ac279
Ibrahim-2003 <ialakash@live.com>
2022-02-06 17:25:47 -0600

    dsjkcn

--- Mobile Web Frontend/login.js
+++ Mobile Web Frontend/login.js
@@ -502,7 +502,9 @@ app.get("/host", checkAuthenticated, function(req, res){
                 res.redirect('/host_signup');
             }else{
                 var tourneys = await getHostedTourneys(user_id);
-                // console.log(tourneys[0])
+                for (tour of tourneys){
+                    console.log(tour);
+                }
                 res.render('host_home.ejs', {
                     tourneys: tourneys,
                     tourney_path: venue_path+'/'
